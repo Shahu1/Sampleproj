76f288ba38a53452270b5e0d218239b670b10027
Shahu1 <Shahistamaneri08@gmail.com>
2022-10-08 20:35:46 +0530

    adding changes made by poonam in demo.java

--- javaproject/src/javaproject/Demo.java
+++ javaproject/src/javaproject/Demo.java
@@ -3,6 +3,6 @@ package javaproject;
 class animal{
 	public static void main(String args[]){
 		System.out.print("hello world");
-		system.out.print("hey this is poonam");
+		System.out.print("hey this is poonam");
 	}
 }
